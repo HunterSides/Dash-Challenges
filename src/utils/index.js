8460d13079045cd91680fef7dc93f46b121c2b4a
Hunter Sides <56932084+HunterSides@users.noreply.github.com>
2022-01-21 17:57:10 -0600

    removed unnecessary files

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,4 @@
 export { isAuthenticated } from './auth'
-export { init } from './init'
 export { getDashAccount, registerIdentity } from './dash'
 export { getMnemonic } from './storage'
 export { getAvatarString } from './string'
